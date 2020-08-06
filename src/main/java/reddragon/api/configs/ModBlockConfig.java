876d4d41c54191c41d5b5196661803b18a0a1d50
Sophia Müller <sophiamueller3000@yahoo.de>
2020-08-06 16:37:54 +0200

    Fixed block config not creatig an item for registered blocks

--- src/main/java/reddragon/api/configs/ModBlockConfig.java
+++ src/main/java/reddragon/api/configs/ModBlockConfig.java
@@ -4,6 +4,9 @@ import java.util.Locale;
 
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import reddragon.api.content.BlockHolder;
@@ -20,11 +23,11 @@ public class ModBlockConfig implements BlockHolder {
 		this(new Block(settings));
 	}
 
-	public void register(final String namespace, final String name) {
+	public void register(final String namespace, final String name, ItemGroup itemGroup) {
 		final Identifier identifier = new Identifier(namespace, name.toLowerCase(Locale.ROOT));
 
 		Registry.register(Registry.BLOCK, identifier, block);
-
+		Registry.register(Registry.ITEM, identifier, new BlockItem(block, new Item.Settings().group(itemGroup)));
 	}
 
 	@Override
